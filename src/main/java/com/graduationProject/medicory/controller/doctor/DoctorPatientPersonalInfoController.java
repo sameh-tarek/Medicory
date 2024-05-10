1ed9dba4d863898246d933c66cdcfe6a065487f3
sameh-tarek <tarksame7@gmail.com>
2024-05-10 18:35:34 +0300

    doctor -> add summary for DoctorPatientPersonalInfo & DoctorPrescription controllers  endpoints

--- src/main/java/com/graduationProject/medicory/controller/doctor/DoctorPatientPersonalInfoController.java
+++ src/main/java/com/graduationProject/medicory/controller/doctor/DoctorPatientPersonalInfoController.java
@@ -3,6 +3,7 @@ package com.graduationProject.medicory.controller.doctor;
 import com.graduationProject.medicory.model.patient.PatientPersonalInformation;
 import com.graduationProject.medicory.service.doctor.DoctorMedicalHistoryService;
 import com.graduationProject.medicory.service.doctor.DoctorPatientPersonalInfoService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ public class DoctorPatientPersonalInfoController {
 
     private final DoctorPatientPersonalInfoService doctorPatientPersonalInfoService;
 
+    @Operation(summary = "Doctor Get Patient Personal Info")
     @GetMapping("personal-information")
     public PatientPersonalInformation getPatientPersonalInformation(@PathVariable String userCode){
          return doctorPatientPersonalInfoService.getPatientPersonalInformation(userCode);
