416075453a3cf46b077b4a9ab1ce18e38584ce16
sameh-tarek <tarksame7@gmail.com>
2024-05-20 21:03:27 +0300

    edit pharmacy to return more info about prescription

--- src/main/java/com/graduationProject/medicory/controller/pharmacy/PharmacyPrescriptionController.java
+++ src/main/java/com/graduationProject/medicory/controller/pharmacy/PharmacyPrescriptionController.java
@@ -1,6 +1,6 @@
 package com.graduationProject.medicory.controller.pharmacy;
 
-import com.graduationProject.medicory.model.prescription.PrescriptionResponse;
+import com.graduationProject.medicory.model.prescription.PrescriptionResponseDTO;
 import com.graduationProject.medicory.service.pharmacy.PharmacyPrescriptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,29 +14,22 @@ import java.util.List;
 @RequestMapping("/pharmacy")
 public class PharmacyPrescriptionController {
     private final PharmacyPrescriptionService pharmacyPrescriptionService;
+
     @GetMapping("{userCode}/prescriptions")
-    ResponseEntity<List<PrescriptionResponse>> getAllPrescription(@PathVariable String userCode){
-        List<PrescriptionResponse> allPrescriptionsResponse = pharmacyPrescriptionService.getAllPrescription(userCode);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(allPrescriptionsResponse);
+    public ResponseEntity<List<PrescriptionResponseDTO>> getAllPrescription(@PathVariable String userCode) {
+        List<PrescriptionResponseDTO> allPrescriptionsResponse = pharmacyPrescriptionService.getAllPrescription(userCode);
+        return ResponseEntity.status(HttpStatus.OK).body(allPrescriptionsResponse);
     }
+
     @GetMapping("{userCode}/active-prescriptions")
-    ResponseEntity<List<PrescriptionResponse>> getActivePrescription(@PathVariable String userCode){
-        List<PrescriptionResponse> allPrescriptionsResponse = pharmacyPrescriptionService.getActivePrescription(userCode);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(allPrescriptionsResponse);
+    public ResponseEntity<List<PrescriptionResponseDTO>> getActivePrescription(@PathVariable String userCode) {
+        List<PrescriptionResponseDTO> allPrescriptionsResponse = pharmacyPrescriptionService.getActivePrescription(userCode);
+        return ResponseEntity.status(HttpStatus.OK).body(allPrescriptionsResponse);
     }
+
     @GetMapping("{userCode}/prescriptions/{id}")
-    ResponseEntity<PrescriptionResponse> getPrescriptionById(@PathVariable String userCode, @PathVariable Long id){
-        PrescriptionResponse prescription = pharmacyPrescriptionService.getPrescriptionById(userCode,id);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(prescription);
+    public ResponseEntity<PrescriptionResponseDTO> getPrescriptionById(@PathVariable String userCode, @PathVariable Long id) {
+        PrescriptionResponseDTO prescription = pharmacyPrescriptionService.getPrescriptionById(userCode, id);
+        return ResponseEntity.status(HttpStatus.OK).body(prescription);
     }
-
 }
